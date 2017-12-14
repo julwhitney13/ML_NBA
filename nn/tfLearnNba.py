e52b71d9d2e84aaa67c6955a0a7db74d8e75686b
Kim <whitney.k@husky.neu.edu>
2017-12-14 11:31:04 -0500

    final predictions and model

--- nn/tfLearnNba.py
+++ nn/tfLearnNba.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import numpy as np
 import tflearn
 from tflearn.data_utils import load_csv
+import csv
+
 
 team_dict = {
     'Philadelphia 76ers': 1,
@@ -62,7 +64,7 @@ def validation_stats(game_file="GameResults_15-16.csv", team_file="TeamStats_15-
     for l in game_results:
         team_num = team_dict.get(l[0])
         opponent_num  = team_dict.get(l[1])
-        new_l = team_stats.get(team_num) + opponent_stats.get(opponent_num)
+        new_l = team_stats.get(team_num) + team_stats.get(opponent_num)
         results.append(new_l)
     return results, labels
 
@@ -70,35 +72,40 @@ year1516_data, year1516_labels = validation_stats()
 year1617_data, year1617_labels = validation_stats("GameResults_16-17.csv", "TeamStats_16-17.csv", "OpponentStats_16-17.csv")
 year1415_data, year1415_labels = load_csv('Game1415.csv', target_column=0, categorical_labels=True, n_classes=2)
 
-data = np.concatenate((year1415_data, year1516_data))
-labels = np.concatenate((year1415_labels, year1516_labels))
-val_results = year1617_data
-val_labels = year1617_labels
+data = np.concatenate((year1415_data, year1617_data))
+labels = np.concatenate((year1415_labels, year1617_labels))
+val_results = year1516_data
+val_labels = year1516_labels
 
 # Build neural network
 net = tflearn.input_data(shape=[None, len(data[0])])
-fc1 = tflearn.fully_connected(net, 32, trainable=True)
-fc2 = tflearn.fully_connected(fc1, 32, trainable=True)
-fc3 = tflearn.fully_connected(fc2, 2, activation='softmax')
-net = tflearn.regression(fc3, optimizer='adam')
+fc1 = tflearn.fully_connected(net, 32, trainable=True, bias=True)
+fc2 = tflearn.fully_connected(fc1, 32, trainable=True, bias=True)
+fc22 = tflearn.fully_connected(fc2, 32, trainable=True, bias=True)
+fc3 = tflearn.fully_connected(fc22, 2, activation='softmax', bias=True)
+adagrad = tflearn.optimizers.AdaGrad(learning_rate=0.01, initial_accumulator_value=0.01)
+net = tflearn.regression(fc3, optimizer=adagrad)
 
 # Define model
 model = tflearn.DNN(net, tensorboard_verbose=3)
 
 # model.get_weights(fc2.W)
 # model.set_weights(fc2.W, np.random.rand(32, 32))
+# year1718_data, _ = validation_stats("GameResults_17-18.csv", "TeamStats_17-18.csv", "OpponentStats_17-18.csv")
 
-model.fit(data, labels, n_epoch=30, batch_size=16, show_metric=True, validation_set=(val_results, val_labels), validation_batch_size=16)
+model.fit(data, labels, n_epoch=50, batch_size=16, show_metric=True, validation_set=(val_results, val_labels), validation_batch_size=16)
 
 # validate, _ = preprocess()
 
-# pred = model.predict(validate)
-# i = 0
-# for l in validate:
-#     print(l[0], "vs", l[13], pred[i][1])
-#     i = i + 1
-#
-# print("", pred[0][1])
-# print("cavs v bulls:", pred[1][1])
-# print("pel v warriors: ", pred[2][1])
-# print(pred)
+def home_team_wins(pred):
+    if pred < .500:
+        return 1
+    else:
+        return 0
+
+pred = model.predict(val_results)
+
+# with open('1516PredictionsForReal.csv', 'wb') as csvfile:
+#     writer = csv.writer(csvfile)
+#     for row in pred:
+#         writer.writerow([home_team_wins(row[1])])
